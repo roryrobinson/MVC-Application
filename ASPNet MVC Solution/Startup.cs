a077a401d66ad0fc6f5f5bc5e8fb62a6259b13aa
Rory Robinson <rory.robinson@d-fine.co.uk>
2018-04-27 16:57:24 +0100

    Add project files.

---
+++ ASPNet MVC Solution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNet_MVC_Solution.Startup))]
+namespace ASPNet_MVC_Solution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
